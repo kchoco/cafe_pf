73333d04808dbfc2793be2c0d11bb0c3ed683ca7
kchoco <kyoka.n.0127@gmail.com>
2021-08-09 15:21:49 +0000

    firstcommit

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  has_many :post_images, dependent: :destroy
+
   def active_for_authentication?
     super && (self.is_valid == false)
   end
