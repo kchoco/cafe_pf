fb1ef2b3d879eda7a9836bd2da270004a43bf747
kchoco <kyoka.n.0127@gmail.com>
2021-08-14 13:35:43 +0000

    フォロー機能追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,11 +4,35 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+#画像投稿機能
   has_many :post_images, dependent: :destroy
+#いいね機能
   has_many :favorites, dependent: :destroy
+#コメント投稿機能
   has_many :post_comments, dependent: :destroy
 
+#退会処理
   def active_for_authentication?
     super && (self.is_valid == false)
   end
+
+  #フォロー機能
+  has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+  has_many :followers, through: :reverse_of_relationships, source: :follower
+  # 被フォロー関係を通じて参照→followed_idをフォローしている人
+
+  has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  has_many :followings, through: :relationships, source: :followed
+  # 与フォロー関係を通じて参照→follower_idをフォローしている人
+
+  def follow(user_id)
+    relationships.create(followed_id: user_id)
+  end
+  def unfollow(user_id)
+    relationships.find_by(followed_id: user_id).destroy
+  end
+  def following?(user)
+    followings.include?(user)
+  end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_12_055858) do
+ActiveRecord::Schema.define(version: 2021_08_13_064809) do
 
   create_table "favorites", force: :cascade do |t|
     t.integer "user_id"
@@ -33,6 +33,13 @@ ActiveRecord::Schema.define(version: 2021_08_12_055858) do
     t.text "introduction"
     t.float "rate"
     t.string "image_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "relationships", force: :cascade do |t|
+    t.integer "follower_id"
+    t.integer "followed_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
