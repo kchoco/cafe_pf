ff0ac819eb8928f63750ec2be2df234d47139daa
kchoco <kyoka.n.0127@gmail.com>
2021-08-18 11:46:27 +0000

    secondcommit

--- db/migrate/20210808131100_add_profile_image_to_uses.rb
+++ db/migrate/20210808131100_add_profile_image_to_uses.rb
@@ -2,7 +2,6 @@ class AddProfileImageToUses < ActiveRecord::Migration[5.2]
   def change
     add_column :users, :profile_image_id, :string
     add_column :users, :profile, :text
-    add_column :users, :nickname, :string
     add_column :users, :is_valid, :boolean, default: true, null: false
   end
 end
