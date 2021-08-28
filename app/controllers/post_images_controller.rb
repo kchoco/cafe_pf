42b0cbce69fc56aef1562abd07a5017b661107c2
kchoco <kyoka.n.0127@gmail.com>
2021-08-28 04:43:46 +0000

    タグ機能修正

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -27,7 +27,7 @@ class PostImagesController < ApplicationController
     else
       @post_images = PostImage.all.order(created_at: :desc)
     end
-    @tags = Tag.select(:name).limit(10).distinct
+    @tags = Tag.limit(10).distinct.order(created_at: :desc)
   end
 
   def show
