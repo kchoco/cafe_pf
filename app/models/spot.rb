27387d6c0e784eb5bed3515056d6735ccdb92233
kchoco <kyoka.n.0127@gmail.com>
2021-08-30 12:14:47 +0000

    タグ機能修正

--- app/models/spot.rb
+++ app/models/spot.rb
@@ -1,8 +1,7 @@
 class Spot < ApplicationRecord
   belongs_to :post_image
 
-  validates :latitude, presence: true
-  validates :longitude, presence: true
+  validates :address, presence: true
 
   geocoded_by :address
   after_validation :geocode
